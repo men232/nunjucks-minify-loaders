545bc1787fb6ea6c9764a8f97fcde5bf85a1ad07
Felipe Matos Santana <felipems@yahoo.com.br>
2018-05-25 09:42:49 -0300

    Improve default ignoreCustomFragments

--- test/page.js
+++ test/page.js
@@ -14,8 +14,8 @@ describe('page.html view', () => {
 		var env = new nunjucks.Environment(loader);
 
 		assert.equal(
-			env.render('page.html', { title: 'Page title', description:'Page description','stylesheets':["/assets/style.css"],'javascripts':["/assets/main.js"], 'bodyClass':'myBodyClass' }),
-			'<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8"><title>Page title</title><meta name="description" content="Page description">  <link rel="stylesheet" type="text/css" href="/assets/style.css">   <script type="text/javascript" src="/assets/main.js"></script>  <style type="text/css">body{color:red}</style></head><body  class="myBodyClass"  ><script type="text/javascript">var toto="";console.log(toto)</script>  sample body  </body></html>',
+			env.render('page1.html', { title: 'Page title', description:'Page description','stylesheets':["/assets/style.css"],'javascripts':["/assets/main.js"], 'bodyClass':'myBodyClass' }),
+			'<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8"><title>Page title</title><meta name="description" content="Page description"><link rel="stylesheet" type="text/css" href="/assets/style.css"><script type="text/javascript" src="/assets/main.js"></script><style type="text/css">body{color:red}</style></head><bodyclass="myBodyClass"><script type="text/javascript">var toto="";console.log(toto)</script>sample body</body></html>',
 		);
 	});
 
@@ -23,16 +23,41 @@ describe('page.html view', () => {
 		var loader = new FileMinifyLoader(viewPath, Object.assign({} , opts, {
 			minify: {
 				removeComments: false,
-				collapseWhitespace: true,
-				minifyJS: true,
-				minifyCSS: true,
 			},
 		}));
 		var env = new nunjucks.Environment(loader);
 
 		assert.equal(
-			env.render('page.html', { title: 'Page title', description:'Page description','stylesheets':["/assets/style.css"],'javascripts':["/assets/main.js"], 'bodyClass':'myBodyClass' }),
-			'<!DOCTYPE html><html lang="fr"><head><meta charset="utf-8"><title>Page title</title><meta name="description" content="Page description">  <link rel="stylesheet" type="text/css" href="/assets/style.css">   <script type="text/javascript" src="/assets/main.js"></script>  <style type="text/css">body{color:red}</style></head><body  class="myBodyClass"  ><!--This is a comment--><script type="text/javascript">var toto="";console.log(toto)</script>  sample body  </body></html>',
+			env.render('page2.html'),
+			'<!--comment-->\n',
+		);
+	});
+
+	it('is ignoring nunjucks variable fragments using default ignoreCustomFragments', () => {
+		var loader = new FileMinifyLoader(viewPath, Object.assign({} , opts, {
+			minify: {
+				sortClassName: true,
+			},
+		}));
+		var env = new nunjucks.Environment(loader);
+
+		assert.equal(
+			env.render('page3.html'),
+			'<div class="bar"></div>\n',
+		);
+	});
+
+	it('is ignoring nunjucks comment fragments using default ignoreCustomFragments', () => {
+		var loader = new FileMinifyLoader(viewPath, Object.assign({} , opts, {
+			minify: {
+				sortAttributes: true,
+			},
+		}));
+		var env = new nunjucks.Environment(loader);
+
+		assert.equal(
+			env.render('page4.html'),
+			'<div></div>\n',
 		);
 	});
 });
